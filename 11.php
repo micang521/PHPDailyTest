4532bd22a8c40c6a8bddd27477f83de1ece33b21
micang521 <960885911@qq.com>
2019-05-23 09:34:40 +0800

    第十一单元

--- 11.php
+++ 11.php
@@ -2,16 +2,9 @@
 
 $str = "student. a am I";
 function test($str){
-    $arr = [];
-    $len = strlen($str);
-    for ($i=0;$i<$len;$i++){
-        if(isset($arr[$str[$i]])){
-            $arr[$str[$i]]++;
-        }
-        else{
-            $str1 = strrev($str);
-            return $str1;
-        }
-    }
+    $arr = explode(" ",$str);
+    $arr1 = array_reverse($arr);
+    $arr2 = implode($arr1," ");
+    return $arr2;
 }
 print_r(test($str));
